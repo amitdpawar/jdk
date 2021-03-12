0b3fb67502b4e2f50bc9a24f03f052fa09733c1f
Amit Pawar <amith.pawar@gmail.com>
2021-03-13 04:52:28 +0530

    Enables multi-thread pretouch during Old-gen expansion.

--- src/hotspot/share/gc/shared/pretouchTask.hpp
+++ src/hotspot/share/gc/shared/pretouchTask.hpp
@@ -30,14 +30,50 @@
 class PretouchTask : public AbstractGangTask {
   char* volatile _cur_addr;
   char* const _start_addr;
-  char* const _end_addr;
+  char* volatile _end_addr;
   size_t _page_size;
   size_t _chunk_size;
+  volatile uint _n_threads;  // Number of threads participating in pretouch.
 
 public:
-  PretouchTask(const char* task_name, char* start_address, char* end_address, size_t page_size, size_t chunk_size);
+  enum TaskStatus{ NotReady, Ready, Done};
+private:
+  // This is used to prevent threads accessing other members while object
+  // is getting updated from expander thread. Then other threads wants to
+  // join pretouch can check this flag and return.
+  volatile char buf[8];
+  volatile size_t _task_status;
+
+public:
+  PretouchTask(const char* task_name, char* start_address, char* end_address, size_t page_size, size_t chunk_size, size_t n_threads = 0, size_t task_status = Ready);
+
+  void reinitialize(char* start_address, char *end_addr);
+  //void set_end_addr(char* end_addr)       { Atomic::release_store(&_end_addr, end_addr); }
+  //void set_cur_addr(char* cur_addr)       { Atomic::release_store(&_cur_addr, end_addr); }
+
+  void set_task_status(TaskStatus status) { Atomic::release_store(&_task_status, (size_t)status);  }
+  void set_task_done()                    { Atomic::release_store(&_task_status, (size_t)Done);    }
+  void set_task_ready()                   { set_task_status(Ready);    }
+  void set_task_notready()                { set_task_status(NotReady); }
+  bool is_task_ready()                    { return Atomic::load(&_task_status) == Ready; }
+  bool is_task_done()                     { return Atomic::load(&_task_status) == Done;  }
 
   virtual void work(uint worker_id);
+
+/*
+  void print(size_t thread_id) {
+    char *sa = Atomic::load(&_start_addr);
+    char *ca = Atomic::load(&_cur_addr);
+    char *ea = Atomic::load(&_end_addr);
+    log_trace(gc,heap)( "   Spinner: thread_id=%ld, _start_addr=%p, _cur_addr=%p, _end_addr=%p, _page_size=%ld, _chunk_size=%ld ", thread_id, sa, ca, ea, _page_size, _chunk_size);
+  }
+*/
+
+  static void* operator new(size_t size) throw() {
+    return CHeapObj<mtGC>::operator new(size);
+  }
+
+  static void setup_chunk_size_and_page_size(size_t& chunk_size, size_t& page_size);
 
   static size_t chunk_size();
 
